8d43d82642829328303c2bf20a9b704f01c7b6a7
Martin Isaksson <martin.isaksson@ericsson.com>
2014-08-08 15:46:26 +0200

    Update download and preprocess.

--- download_and_preprocess.R
+++ download_and_preprocess.R
@@ -1,21 +1,33 @@
+# This was needed since I had Swedish set.
+# Sys.setlocale("LC_ALL", 'en_US.UTF-8')
+
+library(sqldf)
+
 data_url <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
 filename <- "exdata_data_household_power_consumption.zip"
 
 if (!file.exists(filename)){
+  cat("File did not exist, downloading...\n")
   download.file(data_url, filename, method="curl")
   unzip(filename)
 }
 
-power <- read.csv2("household_power_consumption.txt", stringsAsFactors=F,na.strings="?")
-power$date <- as.POSIXct(paste(power$Date, power$Time), format="%d/%m/%Y %H:%M:%S")
-power$Global_active_power <- as.numeric(power$Global_active_power)
-power <- power[as.Date(power$date) == as.Date("2007-02-01") | as.Date(power$date) == as.Date("2007-02-02"),]
+cat("Reading data...\n")
+# Only read dates 2007-02-01 and 2007-02-02. Date format is d/m/Y.
+dataset <- read.csv.sql("household_power_consumption.txt",
+	sep=";", 
+	stringsAsFactors=F,
+ 	sql='select * from file where Date="1/2/2007" OR Date="2/2/2007"')
 
-# Plot1
-hist(as.numeric(power$Global_active_power), ylab="Frequency", xlab="Global Active Power (kilowatts)", breaks=12, col="red", main="Global Active Power")
+# Merge together date and time to creata POSIXct vector. Keep Date and Time separate 
+# so that it's easier to find bugs later.
+dataset$date <- as.POSIXct(paste(dataset$Date, dataset$Time), format="%d/%m/%Y %H:%M:%S")
 
-#Plot 2
-png("plot1.png", width=480, height=480, units="px")
-plot(power$date, as.numeric(power$Global_active_power), type="n", xlab="", ylab="Global Active Power (kilowatts)")
-lines(power$date, as.numeric(power$Global_active_power))
-dev.off()
+# Convert character to numeric.
+dataset$Global_active_power <- as.numeric(dataset$Global_active_power)
+dataset$Sub_metering_1 <- as.numeric(dataset$Sub_metering_1)
+dataset$Sub_metering_2 <- as.numeric(dataset$Sub_metering_2)
+dataset$Sub_metering_3 <- as.numeric(dataset$Sub_metering_3)
+dataset$Voltage <- as.numeric(dataset$Voltage)
+dataset$Global_reactive_power <- as.numeric(dataset$Global_reactive_power)
+
